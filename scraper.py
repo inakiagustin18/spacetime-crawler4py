6c9ee08d9e647238f95855277471a8ec0ad8d387
Inaki Gacula Agustin <iagustin@circinus-25.ics.uci.edu>
2023-04-27 12:41:20 -0700

    Updated how robots.txt files are stored by using shelves instead of global variables.
    Updated how dead urls are checked.
    
    Reordered the code in is_valid function

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 import urllib.robotparser
 from utils import get_urlhash
-from collections import Counter
 
 # ********** HELPER FUNCTIONS **********
 # The tokenize function runs in linear-time relative to the number of words in the text O(n)
@@ -49,13 +48,6 @@ def process_report(file_name):
     with shelve.open(file_name) as shelve_file:
         print(f"1. Number of unique pages: {len(shelve_file)}")
         print(f"2. Longest page in terms of the number of words: {shelve_file[max(shelve_file, key=lambda x: len(shelve_file[x][3]))][0]}")
-        # base_dict = dict()
-        # combined_frequencies = defaultdict(int)
-        # for tup in shelve_file.values():
-        #     for word in tup[3]:
-        #         combined_frequencies[word] += tup[3][word]
-        # counts = sum((Counter(x[3]) for x in shelve_file.values()), Counter())
-        # print(counts)
         
 # **************************************
 
@@ -75,22 +67,28 @@ def extract_next_links(url, resp):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
 
-    # Errors check status code
+    # Error checks status code.
     if resp.status != 200:
         return list()
 
-    # If redirected, resp.raw_response.content is the redirected content.
     soup = BeautifulSoup(resp.raw_response.content, 'lxml')
-    
+
+    for element in soup(["script", "style"]):
+        element.extract()
+    text = soup.get_text()
+
+    # Handles dead urls.
+    if resp.status == 200 and not text:
+        return list()
+
     urls = []
-    # Iterates through the elements with anchor tag 'a'. soup.find_all('a') returns an iterable that stores the hyperlinks found in the current page.
+    # Iterates through the elements with anchor tag 'a'. soup.find_all('a') returns an iterable that stores the hyperlinks found in the current page. 
+    # hyperlink.get('href') returns the hyperlink's destination, which could be a relative/absolute url. urljoin handles the case where the hyperlink's destination is a relative url.
+    # Appends the absolute url into the urls list. absolute_url.split('#')[0] removes fragment from url.
     for hyperlink in soup.find_all('a'):
-        # Appends the absolute url into the urls list. hyperlink.get('href') returns the hyperlink's destination, which could be a relative/absolute url. urljoin handles the case
-        # where the hyperlink's destination is a relative url. absolute_url.split('#')[0] removes fragment from url.
         absolute_url = urljoin(resp.url, hyperlink.get('href'))
         urls.append(absolute_url.split('#')[0])
-    
-    text = soup.get_text()
+
     token_frequency = compute_word_frequencies(tokenize(text))
     with shelve.open('data.shelve') as shelve_file:
         urlhash = get_urlhash(url)
@@ -103,32 +101,29 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
-
-    try:
-        #Using urllib's robot parser module to read through robots.txt file and restrictions
-        currentURL = urllib.robotparser.RobotFileParser()
-        parsed = urlparse(url)
-
-        #Checking if domain is in the list of allowed domains
-        if not re.match(r".*\.(ics|cs|informatics|stat)\.uci\.edu", str(parsed.hostname)):
-            return False
-
-        #Set up the parser with the url of the domain/robots.txt file and read the file
-        currentURL.set_url(parsed.scheme + "://" + parsed.hostname + "/robots.txt")
-        currentURL.read()
-        
-        # #Check if the current url is allowed to be fetch following the robots.txt restrictions
-        if not currentURL.can_fetch("*", url):
-            return False
-    except:
-        # NEED TO SPECIFY ERROR
-        print("Error setting up robots.txt file on %s" %    url)
-        raise
-
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+
+        # Checks if domain is in the list of allowed domains.
+        if not re.match(r".*\.(ics|cs|informatics|stat)\.uci\.edu", str(parsed.hostname)):
+            return False
+        
+        # Set up the parser with the url of the domain/robots.txt file and read the file.
+        with shelve.open('robots.shelve') as shelve_file:
+            if parsed.hostname in shelve_file:
+                robot_parser = shelve_file[parsed.hostname]
+            else:
+                robot_parser = urllib.robotparser.RobotFileParser()
+                robot_parser.set_url(parsed.scheme + "://" + parsed.hostname + "/robots.txt")
+                robot_parser.read()
+                shelve_file[parsed.hostname] = robot_parser
+        
+        # Checks if the current page can be crawled according to its robots.txt.
+        if not robot_parser.can_fetch("*", url):
+            return False
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -138,10 +133,13 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+    except urllib.error.URLError:
+        print(f"{urllib.error.URLError} when setting up robots.txt file for {url}")
+        return False
 
 if __name__ == '__main__':
-    process_report('data.shelve')
+    print(is_valid("https://www.stat.uci.edu/shahbaba-receives-1-7-million-neural-data-analysis-grant"))
+    # process_report('data.shelve')
